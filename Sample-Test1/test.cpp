5a101c2a40f38d28278a133035d5ae925235d7e0
DESKTOP-5FBE16T\User <kjw0384@gmail.com>
2024-06-03 15:17:56 +0900

    2. login

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,10 +4,32 @@
 #include "../Project2/NemoAPI.cpp"
 #include "../Project2/TradingSystem.cpp"
 
+using namespace testing;
+using namespace std;
+
 //1. selectStockBrocker
 TEST(TradingSystemTest, selectStockBroker) {
 	TradingSystem t;
-	t.selectStockBroker();
+	t.selectStockBroker(new KiwiDriver());
 	EXPECT_THAT(t.stockBroker != nullptr);
+}
+
+//2-1. login success
+TEST(TradingSystemTest, loginSuccess) {
+	TradingSystem t;
+	t.selectStockBroker(new KiwiDriver());
+	try {
+		t.login("user", "1234");
+	}
+	catch(exception e){
+		FAIL();
+	}
+}
+
+//2-2. login fail when Broker not selected
+TEST(TradingSystemTest, loginFailWhenBrockerNotSelected) {
+	TradingSystem t;
+	
+	EXPECT_THROW(t.login("user", "1234"), exception);
 }
 
