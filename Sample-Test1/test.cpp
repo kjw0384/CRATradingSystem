653fae182ece94dcc5a41991e0ff7803bf99adf0
DESKTOP-5FBE16T\User <kjw0384@gmail.com>
2024-06-03 14:32:39 +0900

    first commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../Project2/KiwerAPI.cpp"
+#include "../Project2/NemoAPI.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
